3355651ca8a3cc3fdd60cc3920baa12f133656ab
dongyajie <1721929531@qq.com>
2018-04-01 22:19:31 +0800

    新增一个工程

---
+++ Git远程仓库练习1/Git远程仓库练习1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git远程仓库练习1
+//
+//  Created by mac on 18/4/1.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git远程仓库练习1/Git远程仓库练习1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git远程仓库练习1
+//
+//  Created by mac on 18/4/1.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
